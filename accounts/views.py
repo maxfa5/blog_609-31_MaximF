e716d64d3e5299af19fc4af9bfe547083b4faaa3
maxfa5 <maksfa04@mail.ru>
2025-04-12 16:47:54 +0500

    sandajer - add new blocks in about us

--- accounts/views.py
+++ accounts/views.py
@@ -2,15 +2,3 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login, logout
-
-
-def signup_view(request):
-    if request.method =='POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return HttpResponseRedirect('/article')
-    else:
-        form = UserCreationForm()
-    return render(request,'accounts/signup.html', {'form': form})
--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,14 @@
 # from django.http import HttpResponse
 
 from django.shortcuts import render
-
+from articles.models import Article
+from django.templatetags.static import static
 
 def homepage(request):
     data = {"header": "Homepage", "message": "Welcome to my site!"}
-    return render(request, 'homepage.html', context=data)
+    articles = Article.objects.order_by('date')[:2]
+
+    return render(request, 'homepage.html', context={"data": data, "articles":articles})
 
 
 def about(request):
@@ -13,8 +16,9 @@ def about(request):
     staff = ['John Nichols', "John Mirty", 'Timoty Smidth']
     director = {"name": "David Lee", "img": '/director.jpg'}
     address = ('20 W 43 st.', "New York", "NY 1001", 'USA')
-    data = {"header": header,"staff": staff, "director": director, "address":address}
-
+    Contacts = ["https://github.com/maxfa5","@Sandajer","+7........"]
+    data = {"header": header,"staff": staff, "director": director, "address":address, "Contacts":Contacts}
+    img = static("cat.jpg")
     return render(request, 'about.html', data)
 
 from django.shortcuts import render
